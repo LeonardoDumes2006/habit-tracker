9fbb6cfc1672366ac6ab9840325dbf117ab6f088
Leonardo Dumes de Souza <leonardodumesdesouza2006@gmail.com>
2026-09-07 13:55:56 -0300

    feat(Auth): add login funcionality and user session management

---
+++ resources/views/login.blade.php
@@ -0,0 +1,8 @@
+<x-layout>
+    <main class="py-10">
+        <h1 class="text-center">
+           Faça Login
+        </h1> 
+    </main>
+
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Route;
 
@@ -7,4 +8,8 @@ Route::get('/welcome', function () {
     return view('welcome');
 });
 
+// SITE
 Route::get('/', [PageController::class, 'index']);
+
+// LOGIN
+Route::get('/login', [LoginController::class, 'index']); 
